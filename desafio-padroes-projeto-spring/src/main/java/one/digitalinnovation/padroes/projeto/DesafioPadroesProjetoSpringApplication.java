2b99628341c5ea204771d49039c28b069b12df24
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-06-26 10:26:05 -0300

    first commit.

---
+++ desafio-padroes-projeto-spring/src/main/java/one/digitalinnovation/padroes/projeto/DesafioPadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.padroes.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPadroesProjetoSpringApplication.class, args);
+	}
+
+}
