a40ade50fec62883cda9a4ebd56cc613ba45a6ab
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-06-26 13:11:35 -0300

    feat: Added date settings, exception handling and generic record.

---
+++ desafio-padroes-projeto-spring/src/main/java/one/digitalinnovation/padroes/projeto/infra/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.padroes.projeto.infra;
+import one.digitalinnovation.padroes.projeto.utils.ApiGlobalResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    private ResponseEntity<ApiGlobalResponseDto> handlerArgumenteNotValid(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiGlobalResponseDto(errors));
+    }
+
+}
---
+++ desafio-padroes-projeto-spring/src/main/java/one/digitalinnovation/padroes/projeto/utils/ApiGlobalResponseDto.java
@@ -0,0 +1,5 @@
+package one.digitalinnovation.padroes.projeto.utils;
+
+public record ApiGlobalResponseDto (Object response) {
+
+}
